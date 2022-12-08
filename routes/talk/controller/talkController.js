614f43b307b218a285e9c547ab7cd0414d0055e8
geekbutchic <71285487+geekbutchic@users.noreply.github.com>
2022-12-07 23:02:22 -0700

    test

--- routes/talk/controller/talkController.js
+++ routes/talk/controller/talkController.js
@@ -1,58 +1,51 @@
-const Talk = require('../model/Talk');
-const User = require('../../users/model/User');
+const Talk = require("../model/Talk");
+const User = require("../../users/model/User");
 
 module.exports = {
-
   getAllTalks: async (req, res) => {
-
     try {
-      let allTalks = await Talk.find({})
-      console.log('!@-------find-------@!')
-      console.log(allTalks)
-      
-      res.status(200).json(allTalks);                 
+      let allTalks = await Talk.find({});
+      console.log("!@-------find-------@!");
+      console.log(allTalks);
 
+      res.status(200).json(allTalks);
     } catch (error) {
       console.log(error);
       res.status(500).json(error);
     }
-
-
   },
 
   createTalk: async (req, res) => {
-
     let id = req.body.id;
     let talk = req.body.talk;
     let title = req.body.title;
     let image = req.body.image;
 
-    try { 
+    try {
       let foundUser = await User.findById(id);
       let newTalk = await new Talk({
         title: title,
         talk: talk,
-        image: image, 
+        image: image,
         user_id: id
       });
       let savedNewTalk = await newTalk.save();
       await foundUser.talks.push(savedNewTalk);
-      await foundUser.save(); 
+      await foundUser.save();
       res.status(200).json(savedNewTalk);
     } catch (error) {
       res.status(500).json(error);
     }
   },
   getTalkByID: async (req, res) => {
-    const id = req.params.id; 
+    const id = req.params.id;
     try {
-      let foundTalk = await Talk.findById({_id: id});
+      let foundTalk = await Talk.findById({ _id: id });
       res.status(200).json(foundTalk);
     } catch (error) {
-      console.log(error)
+      console.log(error);
       res.status(500).json(error);
     }
-
   },
   deleteByID: async (req, res) => {
     const id = req.params.id;
@@ -60,27 +53,24 @@ module.exports = {
     try {
       let deletedByID = await Talk.findByIdAndRemove(id);
 
-      res.status(200).json(deletedByID)
-
+      res.status(200).json(deletedByID);
     } catch (error) {
-      console.log(error)
+      console.log(error);
       res.status(500).json(error);
     }
-
   },
   getAllUserTalks: async (req, res) => {
-
     const id = req.params.id;
 
     try {
-      let allUserTalks = await User.findById({_id: id}).populate('talks').exec();
+      let allUserTalks = await User.findById({ _id: id })
+        .populate("talks")
+        .exec();
 
-      res.status(200).json(allUserTalks.talks)
-
+      res.status(200).json(allUserTalks.talks);
     } catch (error) {
-      console.log(error)
+      console.log(error);
       res.status(500).json(error);
     }
-
   }
-}
+};
